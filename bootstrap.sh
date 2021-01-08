323b68924e500f0f7173fddfbd39948553cd872d
Stefan Stölzle <stoe@github.com>
2021-01-08 12:57:15 +0100

    🤘 Remove ZSH theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,10 @@ test ! -d "${HOME}/.zgen" && git clone https://github.com/tarjoilija/zgen.git "$
 test -d "${HOME}/.vscode" && rm -rf "${HOME}/.vscode"
 ln -s "${HOME}/dotfiles/.vscode" "${HOME}/.vscode"
 
+test ! -f "${HOME}/.zshrc.codespaces" && ln -s "${HOME}/dotfiles/.zshrc.codespaces" "${HOME}/.zshrc.codespaces"
+
 test -f "${HOME}/.zshrc" && rm "${HOME}/.zshrc"
 ln -s "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
-
-test ! -f "${HOME}/.zsh-theme" && ln -s "${HOME}/dotfiles/.zsh-theme" "${HOME}/.zsh-theme"
-test ! -f "${HOME}/.zshrc.codespaces" && ln -s "${HOME}/dotfiles/.zshrc.codespaces" "${HOME}/.zshrc.codespaces"
 
 test -f "${HOME}/.gitconfig" && rm "${HOME}/.gitconfig"
 ln -s "${HOME}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
