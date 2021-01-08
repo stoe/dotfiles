e074afd54cbcdf9551a4d1a6a95a903cb40c6644
Stefan Stölzle <stoe@github.com>
2021-01-08 12:59:00 +0100

    🤘 Cleanup ZSH

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,6 @@ test ! -d "${HOME}/.zgen" && git clone https://github.com/tarjoilija/zgen.git "$
 test -d "${HOME}/.vscode" && rm -rf "${HOME}/.vscode"
 ln -s "${HOME}/dotfiles/.vscode" "${HOME}/.vscode"
 
-test ! -f "${HOME}/.zshrc.codespaces" && ln -s "${HOME}/dotfiles/.zshrc.codespaces" "${HOME}/.zshrc.codespaces"
-
 test -f "${HOME}/.zshrc" && rm "${HOME}/.zshrc"
 ln -s "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
 
